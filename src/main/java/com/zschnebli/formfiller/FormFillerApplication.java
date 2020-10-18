6686d13afe5a8d10343ab768f9c3ccc6f55322d1
Zoltan Schnebli <Zoltan.Schnebli@cloudflight.io>
2020-10-18 18:20:37 +0300

    Initial commit

---
+++ src/main/java/com/zschnebli/formfiller/FormFillerApplication.java
@@ -0,0 +1,13 @@
+package com.zschnebli.formfiller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormFillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormFillerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zschnebli/formfiller/FormFillerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zschnebli.formfiller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormFillerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
